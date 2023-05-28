8e6f93dce2ca9c257c9f3a12d7c53c5a3bb40db0
Oploz <33009350+oploz@users.noreply.github.com>
2023-05-28 16:02:01 +0200

    Fix

--- Assets/Scripts/Enemies/Mjoelnir_Behavior.cs
+++ Assets/Scripts/Enemies/Mjoelnir_Behavior.cs
@@ -78,7 +78,6 @@ public class Mjoelnir_Behavior : MonoBehaviour
         Spin();
 
         parentTransform.position = new Vector3(parentTransform.position.x, parentTransform.position.y, 0f);
-        transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
     }
 
     // Spins the hammer around
@@ -318,6 +317,7 @@ public class Mjoelnir_Behavior : MonoBehaviour
         rangeIndicator.SetActive(false);
         rangeIndicator.transform.SetParent(parentTransform);
         parentTransform.position = transform.position;
+        transform.position = parentTransform.position;
         chargeParticles.SetActive(false);
         isCharging = false;
 
@@ -341,6 +341,7 @@ public class Mjoelnir_Behavior : MonoBehaviour
         DisableHammer();
         rb.velocity = Vector2.zero;
         parentTransform.position = transform.position;
+        transform.position = parentTransform.position;
 
         // Instantiate circle
         SpriteRenderer sr = aoeIndicator.GetComponent<SpriteRenderer>();
